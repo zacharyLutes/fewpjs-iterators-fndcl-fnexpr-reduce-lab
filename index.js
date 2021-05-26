c49d130ad7b5eef4c6d57ac861622bacd92d9d9b
forkingits1123 <lblack11235813@gmail.com>
2021-05-26 12:52:21 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+let totalBatteries = 0;
 // Code your solution here
+const reducer = () => {
+    for (const item of batteryBatches) {
+        totalBatteries += item;
+    }
+    return totalBatteries;
+}
+win = reducer(batteryBatches);
